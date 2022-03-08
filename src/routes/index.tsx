ebd71cb75a727455a7fd45997a63488d8f1263b0
Enedy <enedy.cordeiro@certponto.com.br>
2022-03-07 22:41:12 -0300

    finalizacao da refatoracao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
+import { Dashboard } from '../pages/Dashboard';
 
-import Dashboard from '../pages/Dashboard';
-
-const Routes = () => (
+const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
   </Switch>
