bf155d9b702dfcdb0a21e858c243a3c83681d9a2
Enedy <enedy.cordeiro@certponto.com.br>
2022-03-07 22:41:24 -0300

    finalizacao da refatoracao

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Food from '../../components/Food';
 import Header from '../../components/Header';
 import { ModalAddFood } from '../../components/ModalAddFood';
